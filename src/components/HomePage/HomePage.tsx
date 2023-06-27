2ff1db40ba89c5da79d55e602ee4ef009b499b7a
czhangy <charleszhangmb@gmail.com>
2023-06-26 22:56:26 -0700

    Basic home page implemented

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,7 +1,20 @@
 import styles from "./HomePage.module.scss";
 
+import { useRouter } from "next/router";
+
 const HomePage: React.FC = () => {
-    return <div className={styles["home-page"]}></div>;
+    const createRun = () => {
+        const router = useRouter();
+        router.push("/run");
+    };
+
+    return (
+        <div className={styles["home-page"]}>
+            <button className={styles["new-button"]} onClick={createRun}>
+                + New Run
+            </button>
+        </div>
+    );
 };
 
 export default HomePage;
