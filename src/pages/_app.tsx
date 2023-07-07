5ea24b6556301ea579fc4a48ff68f5648ebad2e8
Charles Zhang <charleszhangmb@gmail.com>
2023-07-07 11:16:56 +0800

    Make main navbar conditionally rendered

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import Footer from "@/components/Footer/Footer";
 import MainNavbar from "@/components/MainNavbar/MainNavbar";
 import PageWrapper from "@/components/PageWrapper/PageWrapper";
 import "@/styles/globals.scss";
-import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 function App({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+
     return (
         <>
-            <MainNavbar />
+            {router.pathname === "/" ? <MainNavbar /> : ""}
             <PageWrapper>
                 <Component {...pageProps} />
             </PageWrapper>
