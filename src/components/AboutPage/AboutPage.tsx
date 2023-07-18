d26bb9df60901842806f5e1d49c49ad6a3bd21b4
Charles Zhang <charleszhangmb@gmail.com>
2023-07-19 00:05:20 +0800

    Pagination complete

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -1,10 +1,21 @@
+import AboutPagination from "@/components/AboutPagination/AboutPagination";
 import { useState } from "react";
 import styles from "./AboutPage.module.scss";
 
 const AboutPage: React.FC = () => {
+    const pageNames: string[] = ["What is NuzlockeDB?", "What is a Nuzlocke?", "Resources", "Credits"];
+
     const [currentPage, setCurrentPage] = useState<number>(0);
 
-    return <div className={styles["about-page"]}></div>;
+    return (
+        <div className={styles["about-page"]}>
+            <AboutPagination
+                pageNames={pageNames}
+                onSelect={(page: number) => setCurrentPage(page)}
+                currentPage={currentPage}
+            />
+        </div>
+    );
 };
 
 export default AboutPage;
