dc001210ca40421655a08d700be8b6c4e1fc1888
Charles Zhang <charleszhangmb@gmail.com>
2023-07-18 23:35:43 +0800

    AboutPagination

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import styles from "./AboutPage.module.scss";
 
 const AboutPage: React.FC = () => {
+    const [currentPage, setCurrentPage] = useState<number>(0);
+
     return <div className={styles["about-page"]}></div>;
 };
 
