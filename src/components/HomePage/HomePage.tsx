9d8edc6569e6e5fcf6ed7f11c989fdc1e8f43d45
Charles Zhang <charleszhangmb@gmail.com>
2023-07-20 04:31:53 +0800

    Refactor RunList

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,57 +1,42 @@
 import CreateRun from "@/components/CreateRun/CreateRun";
 import Modal from "@/components/Modal/Modal";
-import RunEntry from "@/components/RunEntry/RunEntry";
+import RunList from "@/components/RunList/RunList";
 import { useEffect, useState } from "react";
 import styles from "./HomePage.module.scss";
 
 const HomePage: React.FC = () => {
-    const [createRunOpen, setCreateRunOpen] = useState<boolean>(false);
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
     const [resetFlag, setResetFlag] = useState<number>(0);
-    const [runs, setRuns] = useState<string[]>([]);
+    const [runNames, setRunNames] = useState<string[]>([]);
 
+    // Hacky way to reset the modal contents on close
     const closeCreateRunModal = () => {
-        // Hacky way to reset the modal contents on close
         setTimeout(() => {
             setResetFlag((resetFlag + 1) % 2);
         }, 500);
-        setCreateRunOpen(false);
+        setModalOpen(false);
     };
 
-    const fetchRuns = () => {
-        const storedRuns = localStorage.getItem("runs");
-        if (storedRuns) {
-            let storedRunsList: string[] = JSON.parse(storedRuns);
-            storedRunsList.reverse();
-            setRuns(storedRunsList);
+    // Fetch run names from local storage
+    const getRunNames = () => {
+        const storedRunNamesString: string | null = localStorage.getItem("runs");
+        if (storedRunNamesString) {
+            let storedRunNamesList: string[] = JSON.parse(storedRunNamesString);
+            storedRunNamesList.reverse();
+            setRunNames(storedRunNamesList);
         }
     };
 
     // Fetch existing runs from local storage to display in list
-    useEffect(fetchRuns, []);
+    useEffect(getRunNames, []);
 
     return (
         <>
-            <Modal modalID="create-run-modal" onClose={closeCreateRunModal} open={createRunOpen}>
+            <Modal modalID="create-run-modal" onClose={closeCreateRunModal} open={modalOpen}>
                 <CreateRun key={resetFlag} />
             </Modal>
             <div className={styles["home-page"]}>
-                <div className={styles.runs}>
-                    <div className={styles.header}>
-                        <h2 className={styles.title}>Your Runs</h2>
-                        <button className={styles["new-button"]} onClick={() => setCreateRunOpen(true)}>
-                            + New Run
-                        </button>
-                    </div>
-                    <ul className={styles["run-list"]}>
-                        {runs.length > 0 ? (
-                            runs.map((run: string, key: number) => {
-                                return <RunEntry key={key} onDelete={fetchRuns} run={run} />;
-                            })
-                        ) : (
-                            <p className={styles["alt-text"]}>You don&apos;t have any saved runs yet!</p>
-                        )}
-                    </ul>
-                </div>
+                <RunList runNames={runNames} onDelete={getRunNames} onOpen={() => setModalOpen(true)} />
             </div>
         </>
     );
--- src/components/RunList/RunList.tsx
+++ src/components/RunList/RunList.tsx
@@ -1,7 +1,30 @@
 import styles from "./RunList.module.scss";
+import RunEntry from "@/components/RunEntry/RunEntry";
 
-const RunList: React.FC = () => {
-    return <div className={styles[""]}></div>;
+type Props = {
+    runNames: string[];
+    onDelete: () => void;
+    onOpen: () => void;
+};
+
+const RunList: React.FC<Props> = (props) => {
+    return (
+        <div className={styles["run-list"]}>
+            <h2 className={styles.header}>Your Runs</h2>
+            {props.runNames.length > 0 ? (
+                <ul className={styles.list}>
+                    {props.runNames.map((runName: string, key: number) => {
+                        return <RunEntry onDelete={props.onDelete} runName={runName} key={key} />;
+                    })}
+                </ul>
+            ) : (
+                <p className={styles["alt-text"]}>You don&apos;t have any saved runs yet!</p>
+            )}
+            <button className={styles["new-button"]} onClick={props.onOpen}>
+                + New Run
+            </button>
+        </div>
+    );
 };
 
 export default RunList;
