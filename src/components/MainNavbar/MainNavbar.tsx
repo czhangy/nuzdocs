9d70693ca549dd754ed8482dde935c01ae40b71e
Charles Zhang <charleszhangmb@gmail.com>
2023-07-20 00:18:55 +0800

    Navbar refactor

--- src/components/MainNavbar/MainNavbar.tsx
+++ src/components/MainNavbar/MainNavbar.tsx
@@ -1,31 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./MainNavbar.module.scss";
+import Navbar from "@/components/Navbar/Navbar";
 
 const MainNavbar: React.FC = () => {
     return (
-        <header className={styles["navbar-container"]}>
-            <nav className={styles.navbar}>
-                <div className={styles.branding}>
-                    <div className={styles.logo}>
-                        <Image src="/assets/images/nuzleaf.webp" alt="Logo" layout="fill" objectFit="contain" />
-                    </div>
-                    <h1 className={styles["site-name"]}>NuzlockeDB</h1>
+        <Navbar>
+            <div className={styles.branding}>
+                <div className={styles.logo}>
+                    <Image src="/assets/images/nuzleaf.webp" alt="Logo" layout="fill" objectFit="contain" />
                 </div>
-                <ul className={styles.links}>
-                    <li>
-                        <Link href="/">
-                            <a className={styles.link}>RUNS</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/about">
-                            <a className={styles.link}>ABOUT</a>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+                <h1 className={styles["site-name"]}>NuzlockeDB</h1>
+            </div>
+            <ul className={styles.links}>
+                <li>
+                    <Link href="/">
+                        <a className={styles.link}>RUNS</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/about">
+                        <a className={styles.link}>ABOUT</a>
+                    </Link>
+                </li>
+            </ul>
+        </Navbar>
     );
 };
 
