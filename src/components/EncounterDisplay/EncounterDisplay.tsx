c66b8bcc8fa22d2604b57b1ca5904d06c5028a7b
Charles Zhang <charleszhangmb@gmail.com>
2023-07-23 23:13:06 -0700

    Make display focus while inputting

--- src/components/EncounterDisplay/EncounterDisplay.tsx
+++ src/components/EncounterDisplay/EncounterDisplay.tsx
@@ -12,6 +12,7 @@ type Props = {
 const EncounterDisplay: React.FC<Props> = (props: Props) => {
     const [searchValue, setSearchValue] = useState<string>("");
     const [isSelected, setIsSelected] = useState<boolean>(false);
+    const [isFocused, setIsFocused] = useState<boolean>(false);
 
     // Set encounter display into fail state
     const handleFail = () => {
@@ -26,47 +27,52 @@ const EncounterDisplay: React.FC<Props> = (props: Props) => {
     };
 
     return (
-        <div className={styles["encounter-display"]}>
-            <div className={styles.display}>
-                <div className={styles.sprite}>
-                    <div className={styles["ball-bg"]} />
-                    <hr className={styles["ball-divider"]} />
-                    <div className={styles["ball-center"]} />
-                </div>
-                <div className={styles.text}>
-                    <div className={styles.header}>
-                        <h3 className={styles.title}>Encounter:</h3>
-                        {isSelected ? (
-                            <button className={styles.button} onClick={handleReset}>
-                                <Image
-                                    src="/assets/icons/reset.svg"
-                                    alt="Reset encounter"
-                                    layout="fill"
-                                    objectFit="contain"
-                                />
-                            </button>
-                        ) : (
-                            <button className={styles.button} onClick={handleFail}>
-                                <Image
-                                    src="/assets/icons/x.svg"
-                                    alt="Fail encounter"
-                                    layout="fill"
-                                    objectFit="contain"
-                                />
-                            </button>
-                        )}
+        <>
+            <div className={`${styles["encounter-display"]} ${isFocused ? styles.focused : ""}`}>
+                <div className={`${styles.display}`}>
+                    <div className={styles.sprite}>
+                        <div className={styles["ball-bg"]} />
+                        <hr className={styles["ball-divider"]} />
+                        <div className={styles["ball-center"]} />
                     </div>
-                    <input
-                        className={styles.search}
-                        disabled={isSelected}
-                        type="text"
-                        placeholder="Search..."
-                        onChange={(e) => setSearchValue(e.target.value)}
-                        value={searchValue}
-                    />
+                    <div className={styles.text}>
+                        <div className={styles.header}>
+                            <h3 className={styles.title}>Encounter:</h3>
+                            {isSelected ? (
+                                <button className={styles.button} onClick={handleReset}>
+                                    <Image
+                                        src="/assets/icons/reset.svg"
+                                        alt="Reset encounter"
+                                        layout="fill"
+                                        objectFit="contain"
+                                    />
+                                </button>
+                            ) : (
+                                <button className={styles.button} onClick={handleFail}>
+                                    <Image
+                                        src="/assets/icons/x.svg"
+                                        alt="Fail encounter"
+                                        layout="fill"
+                                        objectFit="contain"
+                                    />
+                                </button>
+                            )}
+                        </div>
+                        <input
+                            className={styles.search}
+                            disabled={isSelected}
+                            type="text"
+                            placeholder="Search..."
+                            onChange={(e) => setSearchValue(e.target.value)}
+                            value={searchValue}
+                            onFocus={() => setIsFocused(true)}
+                            onBlur={() => setIsFocused(false)}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className={`${styles.overlay} ${isFocused ? styles.show : ""}`} />
+        </>
     );
 };
 
