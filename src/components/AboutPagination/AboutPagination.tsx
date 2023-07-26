ad3197efcbe6ff4e0e5e7b7a4e0d627e8aca2f4e
Charles Zhang <charleszhangmb@gmail.com>
2023-07-26 14:51:55 -0700

    About pagination cleanup

--- src/components/AboutPagination/AboutPagination.tsx
+++ src/components/AboutPagination/AboutPagination.tsx
@@ -11,16 +11,16 @@ const AboutPagination: React.FC<Props> = (props: Props) => {
         <ul className={styles["about-pagination"]}>
             {props.pageNames.map((name: string, key: number) => {
                 return (
-                    <li className={styles["page-entry"]} key={key}>
+                    <li className={styles.entry} key={key}>
                         <button
-                            className={`${styles["page-button"]} ${props.currentPage === key ? styles.active : ""}`}
+                            className={`${styles.button} ${props.currentPage === key ? styles.active : ""}`}
                             onClick={() => props.onSelect(key)}
                         >
-                            <p className={styles["page-num"]}>
+                            <p className={styles.num}>
                                 {key + 1}
                                 <span className={styles.underline} />
                             </p>
-                            <p className={styles["page-name"]}>{name}</p>
+                            <p className={styles.name}>{name}</p>
                         </button>
                     </li>
                 );
