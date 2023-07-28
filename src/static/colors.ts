9edd0dd9780cdd51cd76eb84c02dd111b6a72c33
Charles Zhang <charleszhangmb@gmail.com>
2023-07-28 15:53:40 -0700

    Color updates

--- src/static/colors.ts
+++ src/static/colors.ts
@@ -1,9 +1,9 @@
 const colors: { [category: string]: { [identifier: string]: string } } = {
     tiers: {
-        S: "#c44dc4",
+        S: "#7e59c9",
         A: "#086314",
-        B: "#709984",
-        C: "#a8b156",
+        B: "#7ec959",
+        C: "#e3bf52",
         D: "#db9335",
         F: "#c44343",
         "N/A": "#111111",
