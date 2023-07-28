12efcfc148a49f453511aa933c22be1e8b8cc683
Charles Zhang <charleszhangmb@gmail.com>
2023-07-28 16:06:58 -0700

    Consolidate translations into single static file

--- src/static/encounterMethods.ts
+++
@@ -1,9 +0,0 @@
-const encounterMethods: { [key: string]: string } = {
-    walk: "Walking",
-    surf: "Surf",
-    "old-rod": "Old Rod",
-    "good-rod": "Good Rod",
-    "super-rod": "Super Rod",
-};
-
-export default encounterMethods;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,7 @@
 import Run from "@/models/Run";
-import encounterConditions from "@/static/encounterConditions";
-import encounterMethods from "@/static/encounterMethods";
 import tiers from "@/static/tiers";
 import { Name, NamedAPIResource } from "pokenode-ts";
+import translations from "@/static/translations";
 
 export const getEnglishName: (names: Name[]) => string = (names: Name[]): string => {
     const nameObj: Name = names.find((name) => name.language.name === "en")!;
@@ -14,14 +13,16 @@ export const getRun = (runName: string): Run => {
 };
 
 export const getEncounterMethodName = (methodSlug: string, conditionValues: NamedAPIResource[]): string => {
-    let methodName: string = methodSlug in encounterMethods ? encounterMethods[methodSlug] : methodSlug;
+    let methodName: string =
+        methodSlug in translations.encounter_methods ? translations.encounter_methods[methodSlug] : methodSlug;
     if (conditionValues.length > 0) {
         methodName += "\n(";
         conditionValues.forEach((cv: NamedAPIResource, i: number) => {
             if (i > 0) {
                 methodName += " + ";
             }
-            methodName += cv.name in encounterConditions ? encounterConditions[cv.name] : cv.name;
+            methodName +=
+                cv.name in translations.encounter_conditions ? translations.encounter_conditions[cv.name] : cv.name;
         });
         methodName += ")";
     }
