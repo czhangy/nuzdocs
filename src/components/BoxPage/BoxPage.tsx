274a95dcbcb41470b9ab62c6bd6b6b78d8422187
Charles Zhang <charleszhangmb@gmail.com>
2023-08-02 12:48:07 -0700

    BoxPage setup

--- src/components/BoxPage/BoxPage.tsx
+++ src/components/BoxPage/BoxPage.tsx
@@ -5,7 +5,11 @@ type Props = {
 };
 
 const BoxPage: React.FC<Props> = (props: Props) => {
-    return <div className={styles["box-page"]}></div>;
+    return (
+        <div className={styles["box-page"]}>
+            <h2 className={styles.header}>Your Box</h2>
+        </div>
+    );
 };
 
 export default BoxPage;
