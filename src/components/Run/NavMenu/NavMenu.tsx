dcc9864908c1df3a304d1d4cbe848075b23b1f7e
Charles Zhang <charleszhangmb@gmail.com>
2023-08-12 14:31:43 -0700

    Add overlay and close on scroll

--- src/components/Run/NavMenu/NavMenu.tsx
+++ src/components/Run/NavMenu/NavMenu.tsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./NavMenu.module.scss";
 
 const NavMenu: React.FC = () => {
     // Component state
     const [open, setOpen] = useState<boolean>(false);
 
+    // Named function for event listener callback
+    const closeMenu = (): void => setOpen(false);
+
+    // Close menu on scroll
+    useEffect(() => {
+        document.addEventListener("scroll", closeMenu);
+        return () => {
+            document.removeEventListener("scroll", closeMenu);
+        };
+    }, []);
+
     return (
         <div className={`${styles["nav-menu"]} ${open ? styles.open : ""}`}>
+            <div className={styles.overlay} onClick={() => setOpen(false)} />
             <button className={styles.button} onClick={() => setOpen(!open)}>
                 <hr className={styles.icon} />
                 <hr className={styles.icon} />
