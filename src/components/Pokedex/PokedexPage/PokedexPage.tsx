98aaa0c6d582d5f9b522bd957c397e0036af208c
Charles Zhang <charleszhangmb@gmail.com>
2023-08-12 22:46:28 -0700

    Add base stats to PokedexPage

--- src/components/Pokedex/PokedexPage/PokedexPage.tsx
+++ src/components/Pokedex/PokedexPage/PokedexPage.tsx
@@ -4,6 +4,7 @@ import { fetchPokemonFromGroup } from "@/utils/api";
 import { useEffect, useState } from "react";
 import styles from "./PokedexPage.module.scss";
 import EvolutionsDisplay from "@/components/Run/EvolutionsDisplay/EvolutionsDisplay";
+import BaseStats from "@/components/Pokedex/BaseStats/BaseStats";
 
 type Props = {
     group: string;
@@ -24,6 +25,7 @@ const PokedexPage: React.FC<Props> = (props: Props) => {
     return pokemon ? (
         <div className={styles["pokedex-page"]}>
             <PokemonDisplay pokemon={pokemon} group={props.group} />
+            <BaseStats stats={pokemon.stats} />
             {pokemon.evolutions.some((chain: string[]) => chain.length > 1) ? (
                 <EvolutionsDisplay pokemon={pokemon} group={props.group} />
             ) : (
