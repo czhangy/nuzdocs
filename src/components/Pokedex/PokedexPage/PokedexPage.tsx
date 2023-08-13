ebdb1c539df3d159bf9b8bbd8398c0ab0aa87833
Charles Zhang <charleszhangmb@gmail.com>
2023-08-12 23:51:57 -0700

    Add abilities to Pokedex

--- src/components/Pokedex/PokedexPage/PokedexPage.tsx
+++ src/components/Pokedex/PokedexPage/PokedexPage.tsx
@@ -1,13 +1,14 @@
+import AbilityList from "@/components/Pokedex/AbilityList/AbilityList";
 import BaseStats from "@/components/Pokedex/BaseStats/BaseStats";
 import EvolutionsDisplay from "@/components/Run/EvolutionsDisplay/EvolutionsDisplay";
 import PokemonDisplay from "@/components/Run/PokemonDisplay/PokemonDisplay";
+import TierCard from "@/components/Run/TierCard/TierCard";
 import PokemonData from "@/models/PokemonData";
 import Run from "@/models/Run";
 import { fetchPokemon } from "@/utils/api";
+import { getPokemonTier } from "@/utils/utils";
 import { useEffect, useState } from "react";
 import styles from "./PokedexPage.module.scss";
-import TierCard from "@/components/Run/TierCard/TierCard";
-import { getPokemonTier } from "@/utils/utils";
 
 type Props = {
     pokemon: string;
@@ -31,6 +32,7 @@ const PokedexPage: React.FC<Props> = (props: Props) => {
                 <PokemonDisplay pokemon={pokemon} runID={props.run.id} />
                 <TierCard tier={getPokemonTier(props.pokemon, props.run.gameSlug)} />
             </div>
+            <AbilityList abilities={pokemon.abilities} game={props.run.gameSlug} />
             <BaseStats stats={pokemon.stats} />
             {pokemon.evolutions.some((chain: string[]) => chain.length > 1) ? (
                 <EvolutionsDisplay pokemon={pokemon} run={props.run} />
