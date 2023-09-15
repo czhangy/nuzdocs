c0300f32c7e6b8ea1ecf4fe84a88c93b9181d456
Charles Zhang <charleszhangmb@gmail.com>
2023-09-15 00:35:44 -0700

    Wattson split

--- src/static/ruby_sapphire.ts
+++ src/static/ruby_sapphire.ts
@@ -265,6 +265,183 @@ const ruby_sapphire: GameData = {
                 },
             ],
         },
+        {
+            name: "Wattson Split",
+            segments: [
+                {
+                    slug: "hoenn-route-109",
+                    name: "Route 109 (Beach)",
+                    type: "location",
+                    segment: {
+                        battles: [
+                            battles.ruby_sapphire.sailor_huey,
+                            battles.ruby_sapphire.sailor_edmond,
+                            battles.ruby_sapphire.tuber_m_ricky,
+                            battles.ruby_sapphire.tuber_f_lola,
+                            battles.ruby_sapphire.tuber_m_simon,
+                            battles.ruby_sapphire.beauty_johanna,
+                            battles.ruby_sapphire.sailor_dwayne,
+                        ],
+                    },
+                },
+                {
+                    slug: "slateport-city",
+                    name: "Slateport City",
+                    type: "location",
+                    conditions: { game: "ruby" },
+                    segment: {
+                        battles: [
+                            battles.ruby_sapphire.team_magma_m_grunt_3,
+                            battles.ruby_sapphire.team_magma_m_grunt_4,
+                        ],
+                    },
+                },
+                {
+                    slug: "slateport-city",
+                    name: "Slateport City",
+                    type: "location",
+                    conditions: { game: "sapphire" },
+                    segment: {
+                        battles: [battles.ruby_sapphire.team_aqua_m_grunt_3, battles.ruby_sapphire.team_aqua_m_grunt_4],
+                    },
+                },
+                {
+                    slug: "hoenn-route-110",
+                    name: "Route 110 (South)",
+                    type: "location",
+                    segment: { battles: [battles.ruby_sapphire.pokefan_f_isabel] },
+                },
+                {
+                    slug: "hoenn-route-103",
+                    name: "Route 103 (East)",
+                    type: "location",
+                    segment: {
+                        battles: [
+                            battles.ruby_sapphire.aroma_lady_daisy,
+                            battles.ruby_sapphire.twins_amy_and_liv,
+                            battles.ruby_sapphire.pokefan_m_miguel,
+                            battles.ruby_sapphire.fisherman_andrew,
+                        ],
+                    },
+                },
+                {
+                    slug: "trick-house-1",
+                    name: "Trick House 1",
+                    type: "location",
+                    segment: {
+                        battles: [
+                            battles.ruby_sapphire.lass_sally,
+                            battles.ruby_sapphire.lass_robin,
+                            battles.ruby_sapphire.youngster_eddie,
+                        ],
+                        custom: true,
+                    },
+                },
+                {
+                    slug: "hoenn-route-110",
+                    name: "Route 110 (South)",
+                    type: "location",
+                    segment: { battles: [battles.ruby_sapphire.youngster_timmy] },
+                },
+                {
+                    slug: "rival-2",
+                    name: "Rival 2",
+                    type: "battle",
+                    conditions: { character: "brendan", starter: "treecko" },
+                    segment: { battle: battles.ruby_sapphire.may_treecko_2 },
+                },
+                {
+                    slug: "rival-2",
+                    name: "Rival 2",
+                    type: "battle",
+                    conditions: { character: "brendan", starter: "torchic" },
+                    segment: { battle: battles.ruby_sapphire.may_torchic_2 },
+                },
+                {
+                    slug: "rival-2",
+                    name: "Rival 2",
+                    type: "battle",
+                    conditions: { character: "brendan", starter: "mudkip" },
+                    segment: { battle: battles.ruby_sapphire.may_mudkip_2 },
+                },
+                {
+                    slug: "rival-2",
+                    name: "Rival 2",
+                    type: "battle",
+                    conditions: { character: "may", starter: "treecko" },
+                    segment: { battle: battles.ruby_sapphire.brendan_treecko_2 },
+                },
+                {
+                    slug: "rival-2",
+                    name: "Rival 2",
+                    type: "battle",
+                    conditions: { character: "may", starter: "torchic" },
+                    segment: { battle: battles.ruby_sapphire.brendan_torchic_2 },
+                },
+                {
+                    slug: "rival-2",
+                    name: "Rival 2",
+                    type: "battle",
+                    conditions: { character: "may", starter: "mudkip" },
+                    segment: { battle: battles.ruby_sapphire.brendan_mudkip_2 },
+                },
+                {
+                    slug: "hoenn-route-110",
+                    name: "Route 110 (North)",
+                    type: "location",
+                    segment: {
+                        battles: [
+                            battles.ruby_sapphire.collector_edwin,
+                            battles.ruby_sapphire.psychic_m_edward,
+                            battles.ruby_sapphire.fisherman_dale,
+                        ],
+                    },
+                },
+                { slug: "mauville-city", name: "Mauville City", type: "location", segment: { battles: [] } },
+                {
+                    slug: "wally-1",
+                    name: "Wally 1",
+                    type: "battle",
+                    segment: { battle: battles.ruby_sapphire.wally_1 },
+                },
+                {
+                    slug: "cycling-road",
+                    name: "Cycling Road",
+                    type: "location",
+                    segment: {
+                        battles: [
+                            battles.ruby_sapphire.psychic_f_jaclyn,
+                            battles.ruby_sapphire.triathlete_biker_f_abigail,
+                            battles.ruby_sapphire.triathlete_biker_m_anthony,
+                            battles.ruby_sapphire.triathlete_biker_m_benjamin,
+                            battles.ruby_sapphire.triathlete_biker_f_jasmine,
+                            battles.ruby_sapphire.triathlete_biker_m_jacob,
+                        ],
+                        custom: true,
+                    },
+                },
+                {
+                    slug: "mauville-gym",
+                    name: "Mauville Gym",
+                    type: "location",
+                    segment: {
+                        battles: [
+                            battles.ruby_sapphire.guitarist_kirk,
+                            battles.ruby_sapphire.youngster_ben,
+                            battles.ruby_sapphire.guitarist_shawn,
+                            battles.ruby_sapphire.battle_girl_vivian,
+                        ],
+                        custom: true,
+                    },
+                },
+                {
+                    slug: "wattson",
+                    name: "Wattson",
+                    type: "battle",
+                    segment: { battle: battles.emerald.wattson, levelCap: true },
+                },
+            ],
+        },
     ],
 };
 
